16dfee18db313437a50532f75e3e733b49b6efa2
dmitry-poznyak <152187215+dmitry-poznyak@users.noreply.github.com>
2023-12-12 22:39:29 +0300

    Add files via upload

---
+++ 1/Program.cs
@@ -0,0 +1,13 @@
+﻿//Задайте одномерный массив из 10 целых чисел от 1 до 100. 
+//Найдите количество элементов массива, значения которых лежат в отрезке [20,90].
+
+int[] array = {2, 2, 4, 25, 14, 8, 40, 89, 10, 55};
+int num = 20;
+int num1 = 90;
+int result = 0;
+
+for (int i = 0; i < array.Length; i++){
+    if (array[i] >= num && array[i] <= num1)
+    result++;
+}
+Console.WriteLine(result);
