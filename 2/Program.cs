2be44864cba67b691acec701a89d59d31ea5ca31
dmitry-poznyak <152187215+dmitry-poznyak@users.noreply.github.com>
2023-12-25 01:42:57 +0300

    Add files via upload

---
+++ 2/Program.cs
@@ -0,0 +1,49 @@
+﻿// Задача 2: Задайте двумерный массив. Напишите
+// программу, которая поменяет местами первую и
+// последнюю строку массива.
+
+int[,] Construct2DArray(int rows, int colums, int min, int max)
+{
+    int[,] array = new int [rows, colums];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < colums; j++)
+        {
+            array[i, j] = new Random().Next(min, max + 1);
+        }
+    }
+    return array;
+}
+
+void Print2DArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write(array[i, j] + " ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+void SwapRows(int[,] array, int rowI1, int rowI2)
+{
+    int cols = array.GetLength(1);
+    for (int j = 0; j < cols; j++)
+    {
+        int temp = array[rowI1, j];
+        array[rowI1, j] = array[rowI2, j];
+        array[rowI2, j] = temp;
+    }
+}
+
+
+
+
+int[,] myArray = Construct2DArray(5,5,0,9);
+Print2DArray(myArray);
+System.Console.WriteLine();
+SwapRows(myArray, 0, myArray.GetLength(0) - 1);
+Print2DArray(myArray);
+
---
+++ 3/Program.cs
@@ -0,0 +1,55 @@
+﻿// Задача 3: Задайте прямоугольный двумерный массив.
+// Напишите программу, которая будет находить строку с
+// наименьшей суммой элементов.
+
+
+int[,] Construct2DArray(int rows, int colums, int min, int max)
+{
+    int[,] array = new int [rows, colums];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < colums; j++)
+        {
+            array[i, j] = new Random().Next(min, max + 1);
+        }
+    }
+    return array;
+}
+
+void Print2DArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write(array[i, j] + " ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int FindMinSumRow(int[,] array)
+{
+    int minSum = int.MaxValue;
+    int minSumRowIndex = -1;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        int currentSum = 0;
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            currentSum += array[i, j];
+        }
+        if (currentSum < minSum)
+        {
+            minSum = currentSum;
+            minSumRowIndex = i;
+        }
+    }
+    return minSumRowIndex;
+}
+
+int[,] myArray = Construct2DArray(3,6,0,9);
+Print2DArray(myArray);
+System.Console.WriteLine(FindMinSumRow(myArray));
+
+
